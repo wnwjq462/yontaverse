eb80757c356f1f398d8d90729ccd0f0bbc295396
HeoRyang <117929916+HeoRyang@users.noreply.github.com>
2022-11-27 15:48:25 +0900

    Update PlotByVisitTypeQueryService.java

--- src/main/java/org/teemyroom/yontaverse/plot/application/PlotByVisitTypeQueryService.java
+++ src/main/java/org/teemyroom/yontaverse/plot/application/PlotByVisitTypeQueryService.java
@@ -1,15 +1,26 @@
 package org.teemyroom.yontaverse.plot.application;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.teemyroom.yontaverse.common.BaseQueryService;
+import org.teemyroom.yontaverse.common.VisitType;
 import org.teemyroom.yontaverse.plot.domain.Message;
+import org.teemyroom.yontaverse.plot.domain.Plot;
+import org.teemyroom.yontaverse.plot.domain.PlotQueryService;
+import org.teemyroom.yontaverse.plot.domain.PlotRepository;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
-public class PlotByVisitTypeQueryService {
-    public List<Message> query() {
+@Service
+@RequiredArgsConstructor
+public class PlotByVisitTypeQueryService  {
 
+    private final PlotQueryService plotQueryService;
 
-
-        return new ArrayList<Message>();
+    public List<Message> MessageByPlaceIdAndVisitType(Long placeId, VisitType visitType) {
+        return plotQueryService.findByPlaceIdAndVisitType(placeId, visitType).getMessages();
     }
+
 }
